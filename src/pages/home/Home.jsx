8b04645beb6e38f1267eed8549ca97b85d860828
Marlos Ribeiro <marlos.mgrb@gmail.com>
2024-07-18 19:44:57 +0000

    add rodape

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,15 @@
-import Cabecalho from "@/components/cabecalho";
+import Cabecalho from "@/components/Cabecalho";
 import styles from "./Home.module.css";
+import Rodape from "@/components/Rodape";
 
 export function Home() {
-  return <Cabecalho />;
+  return (
+    <>
+      <Cabecalho />
+      <main className={styles.main}>
+        <h1>Home</h1>
+      </main>
+      <Rodape />
+    </>
+  );
 }
