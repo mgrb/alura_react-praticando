59065af999de0dd95c7c21264822a4b511a96bd7
Marlos Ribeiro <marlos.mgrb@gmail.com>
2024-07-19 12:01:47 +0000

    add Cards @Home

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,12 +1,16 @@
-import styles from "./Card.module.css"
-import iconeFavoritar from "./favoritar.png"
+import styles from "./Card.module.css";
+import iconeFavoritar from "./favoritar.png";
 
-export function Card ({id,titulo,capa}) {
-    return (
-        <div className={styles.container}>
-            <img src={capa} alt={titulo} className={styles.capa} />
-            <h2>{titulo}</h2>
-            <img src={iconeFavoritar} alt="Favoritar filme" className={styles.favoritar} />
-        </div>
-    );
+export function Card({ id, titulo, capa }) {
+  return (
+    <div className={styles.container}>
+      <img src={capa} alt={titulo} className={styles.capa} />
+      <h2>{titulo}</h2>
+      <img
+        src={iconeFavoritar}
+        alt="Favoritar filme"
+        className={styles.favoritar}
+      />
+    </div>
+  );
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,17 +1,24 @@
-import Cabecalho from "@/components/Cabecalho";
-import styles from "./Home.module.css";
-import Rodape from "@/components/Rodape";
 import Banner from "@/components/Banner";
-import Titulo from "@/components/Titulo";
+import Cabecalho from "@/components/Cabecalho";
 import Card from "@/components/Card";
+import Rodape from "@/components/Rodape";
+import Titulo from "@/components/Titulo";
+import videos from "@/json/db.json";
+import styles from "./Home.module.css";
 
 export function Home() {
   return (
     <>
       <Cabecalho />
       <Banner imagem="home" />
-      <Titulo><h1>Um Lugar para guardar seus vídeos e filmes!</h1></Titulo>
-      <Card id='1' titulo='teste' capa='https://i.pinimg.com/736x/6f/a0/21/6fa021a998ef2a230ad8251bf1d3c943.jpg'/>
+      <Titulo>
+        <h1>Um Lugar para guardar seus vídeos e filmes!</h1>
+      </Titulo>
+      <section className={styles.container}>
+        {videos.map((video) => (
+          <Card key={video.id} {...video} />
+        ))}
+      </section>
       <Rodape />
     </>
   );
