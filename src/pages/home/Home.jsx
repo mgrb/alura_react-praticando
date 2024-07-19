c0f0adde1b2e46725d428ed7b76acd8e39f1789c
Marlos Ribeiro <marlos.mgrb@gmail.com>
2024-07-19 01:29:10 +0000

    add Card component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,6 +3,7 @@ import styles from "./Home.module.css";
 import Rodape from "@/components/Rodape";
 import Banner from "@/components/Banner";
 import Titulo from "@/components/Titulo";
+import Card from "@/components/Card";
 
 export function Home() {
   return (
@@ -10,6 +11,7 @@ export function Home() {
       <Cabecalho />
       <Banner imagem="home" />
       <Titulo><h1>Um Lugar para guardar seus vídeos e filmes!</h1></Titulo>
+      <Card id='1' titulo='teste' capa='https://i.pinimg.com/736x/6f/a0/21/6fa021a998ef2a230ad8251bf1d3c943.jpg'/>
       <Rodape />
     </>
   );
